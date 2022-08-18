a01e26ba0ff2fd6b17bbd422ab12e121736ad8bd
DungxLas <phamjhungfdungx@gmail.com>
2022-08-18 11:16:25 +0700

    Update App.js

--- 3/App.js
+++ 3/App.js
@@ -1,23 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform, StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Dimensions,
+} from 'react-native';
 
 import SearchInput from './components/SearchInput';
+import getImageForWeather from '../utils/getImageForWeather';
 
 export default function App() {
   return (
     <KeyboardAvoidingView style={styles.container} behavior='padding'>
-      <Text
-        style={[styles.largeText, styles.textStyle]}
-      >San Francisco</Text>
-      <Text
-        style={[styles.smallText, styles.textStyle]}
-      >Light Cloud</Text>
-      <Text
-        style={[styles.largeText, styles.textStyle]}
-      >24°</Text>
-      <SearchInput placeholder='Search any city' />
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={getImageForWeather('Clear')}
+        style={styles.imageContainer}
+        imageStyle={styles.image}
+      >
+        <View
+          style={styles.detailsContainer}
+        >
+          <Text
+            style={[styles.largeText, styles.textStyle]}
+          >San Francisco</Text>
+          <Text
+            style={[styles.smallText, styles.textStyle]}
+          >Light Cloud</Text>
+          <Text
+            style={[styles.largeText, styles.textStyle]}
+          >24°</Text>
+          <SearchInput placeholder='Search any city' />
+          <StatusBar style="auto" />
+        </View>
+      </ImageBackground>
     </KeyboardAvoidingView>
   );
 }
@@ -25,13 +44,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#34495E',
+  },
+  detailsContainer: {
+    //flex: 1,
     justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    //paddingHorizontal: 20,
+    paddingTop: Dimensions.get('screen').height / 2.5,
+  },
+  imageContainer: {
+    Flex: 1,
+  },
+  image: {
+    Flex: 1,
+    Width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').height,
+    resizeMode: 'cover',
   },
   textStyle: {
     textAlign: 'center',
     fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'red',
   },
   largeText: {
     fontSize: 44,
